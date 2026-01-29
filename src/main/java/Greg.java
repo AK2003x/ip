87db105a50b5ad833e8bb6e20c54eb43376cff13
Adam Khoo <adam.khoo.jh@gmail.com>
2026-01-30 01:29:02 +0800

    Add Scanner to read user input in Greg

--- src/main/java/Greg.java
+++ src/main/java/Greg.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 /**
  * Runs the Greg chatbot greeting program.
- * Prints an ASCII logo and a short greeting and farewell to standard output.
+ * Prints an ASCII logo of GREG and a short greeting
+ * Prints an intro message, then repeatedly reads user input and echoes it back.
  */
 public class Greg {
     /**
@@ -18,14 +21,32 @@ public class Greg {
                         + "| |  _  | |_) ||  _|  | |  _  \n"
                         + "| |_| | |  _ < | |___ | |_| | \n"
                         + " \\____| |_| \\_\\|_____| \\____| \n";
-        // Print outputs
+
+        // Greetings & Introduction
         System.out.println(line);
         System.out.println(logo);
         System.out.println(line);
-        System.out.println(" Hello! I'm " + chatbotName);
-        System.out.println(" What can I do for you?");
+        System.out.println(" GREETINGS! I'm " + chatbotName + " :)))");
+        System.out.println(" How can I be of service to you today my good sir!");
         System.out.println(line);
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        // Echo Commands
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String input = sc.nextLine();
+
+            // Checks for "bye" to exit the while loop and output farewell message
+            if (input.equals("bye")) {
+                System.out.println(line);
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+        }
+        sc.close();
     }
 }
