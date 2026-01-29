6ee831a374ef840a0b0ac981396986738c649c32
Adam Khoo <adam.khoo.jh@gmail.com>
2026-01-30 00:27:49 +0800

    level-0

--- src/main/java/Greg.java
+++ src/main/java/Greg.java
@@ -9,10 +9,14 @@ public class Greg {
                         + "| |  _  | |_) ||  _|  | |  _  \n"
                         + "| |_| | |  _ < | |___ | |_| | \n"
                         + " \\____| |_| \\_\\|_____| \\____| \n";
-        //
+        // Print outputs
         System.out.println(line);
         System.out.println(logo);
         System.out.println(line);
         System.out.println(" Hello! I'm " + chatbotName);
+        System.out.println(" What can I do for you?");
+        System.out.println(line);
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
