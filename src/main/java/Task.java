c81d98353269ee3a3800e4b0e5e85154753a54a0
Adam Khoo <adam.khoo.jh@gmail.com>
2026-02-04 21:12:02 +0800

    Add Task Class

---
+++ src/main/java/Task.java
@@ -0,0 +1,31 @@
+/**
+ * The Task class represents a general task with a description.
+ * It is also a base class for similar Classes to inherit from such as Deadline, Todo & event
+ */
+public class Task {
+
+    // The description of the task.
+    // This is a protected field, allowing subclasses to access it.
+    protected String description;
+
+    /**
+     * Constructor for the Task class.
+     * Initialises the task with a specific description.
+     *
+     * @param description The description of the task.
+     */
+    public Task(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Returns a string representation of the task.
+     * By default, it returns the description of the task.
+     *
+     * @return A string representation of the task.
+     */
+    @Override
+    public String toString() {
+        return description; // Returns the description of the task as a string.
+    }
+}
